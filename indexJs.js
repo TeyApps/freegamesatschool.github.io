fbf2f1e7091a5f7c2016d7e360f22331234e0184
TeyApps <tey.kamal9@gmail.com>
2017-01-17 20:39:01 +0000

    Website Upload

---
+++ indexJs.js
@@ -0,0 +1,13 @@
+(function() {
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDP6wRZ1-z0HyABgfXtpMqbdK56t2fx6tk",
+    authDomain: "gamesatschool-ff5a8.firebaseapp.com",
+    databaseURL: "https://gamesatschool-ff5a8.firebaseio.com",
+    storageBucket: "gamesatschool-ff5a8.appspot.com",
+    messagingSenderId: "871418880106"
+  };
+  firebase.initializeApp(config);
+    
+    
+}());
